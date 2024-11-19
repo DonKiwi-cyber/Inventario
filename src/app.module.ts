56ea799f8ae201a4b3170f5cb1671997b4865e92
DonKiwi-cyber <minecraft200294@gmail.com>
2024-11-19 08:32:54 -0600

    basic client

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ClientModule } from './client/client.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, ClientModule],
   controllers: [AppController],
   providers: [AppService],
 })
