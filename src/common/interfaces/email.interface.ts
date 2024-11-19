a9b60b939a5a5742815aa49d1cd94a0dd9efa01f
DonKiwi-cyber <minecraft200294@gmail.com>
2024-11-19 08:22:34 -0600

    common basics

---
+++ src/common/interfaces/email.interface.ts
@@ -0,0 +1,16 @@
+export interface proveedor_email {
+    Fecha: any;
+    Correo: string;
+    Nombre: string;
+    Productos: any;
+    subject: string;
+}
+
+export interface cliente_email {
+    Fecha: any;
+    Correo: string;
+    Cliente: string;
+    Productos: any;
+    Total: number;
+    subject: string;
+}
