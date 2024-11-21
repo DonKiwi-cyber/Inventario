a13210f77a02e7efab10b7ede80a8c1aad59b92f
DonKiwi-cyber <minecraft200294@gmail.com>
2024-11-21 07:13:44 -0600

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ProductosModule } from './productos/productos.module';
 import { CuentasModule } from './resource/cuentas/cuentas.module';
 import { ClientModule } from './client/client.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule, ClientModule, CuentasModule],
+  imports: [AuthModule, ClientModule, CuentasModule, ProductosModule],
   controllers: [AppController],
   providers: [AppService],
 })
