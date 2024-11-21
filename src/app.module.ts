eda9eb1a0ba8605f7819c366f632d4ee2a7297ac
DonKiwi-cyber <minecraft200294@gmail.com>
2024-11-21 07:24:08 -0600

    proveedores resource added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ProveedoresModule } from './resource/proveedores/proveedores.module';
 import { CuentasModule } from './resource/cuentas/cuentas.module';
 import { ClientModule } from './client/client.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule, ClientModule, CuentasModule],
+  imports: [AuthModule, ClientModule, CuentasModule, ProveedoresModule],
   controllers: [AppController],
   providers: [AppService],
 })
